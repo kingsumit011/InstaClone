c943785bad7cf7f57f8646fd45fbcbb5dd720753
Sumit <kingsumit011@gmail.com>
2021-08-07 22:56:12 +0530

    Commit change in font and progress in login and register date 8/7/21 11:00

--- app/src/main/java/com/android/example/instaclone/MainActivity.java
+++ app/src/main/java/com/android/example/instaclone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.example.instaclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 //import android.support.*;
 
@@ -10,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LoginPage Id = new LoginPage();
-        setContentView(R.layout.login_page);
+        startActivity(new Intent());
+//        setContentView(R.layout.login_page);
+
     }
 }
--- app/src/main/java/com/android/example/instaclone/Register.java
+++ app/src/main/java/com/android/example/instaclone/Register.java
@@ -1,11 +1,10 @@
 package com.android.example.instaclone;
 
 import android.os.Bundle;
-//import android.support.annotation.Nullable;
-//import android.support.v7.app.AppCompatActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUp extends AppCompatActivity {
+public class Register extends AppCompatActivity {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
