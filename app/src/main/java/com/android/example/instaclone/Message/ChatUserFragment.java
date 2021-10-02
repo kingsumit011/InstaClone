a014cbb3ba732a026b6f2a8fd1542a6e33f411dd
Sumit <kingsumit011@gmail.com>
2021-10-02 17:08:40 +0530

    Finall Commit

---
+++ app/src/main/java/com/android/example/instaclone/Message/ChatUserFragment.java
@@ -0,0 +1,23 @@
+package com.android.example.instaclone.Message;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.example.instaclone.R;
+
+
+public class ChatUserFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_user, container, false);
+    }
+}
---
+++ app/src/main/java/com/android/example/instaclone/notification/NotificationMessageService.java
@@ -0,0 +1,5 @@
+package com.android.example.instaclone.notification;
+
+public class NotificationMessageService extends FireBaseMessagingService{
+
+}
